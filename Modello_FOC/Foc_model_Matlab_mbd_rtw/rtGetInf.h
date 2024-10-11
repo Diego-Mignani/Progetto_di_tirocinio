f3f81e0d20e67d26dc02a9650458fcc0b21b560f
Diego Mignani <74722720+Diego-Mignani@users.noreply.github.com>
2024-10-11 17:13:39 +0200

    Modello FOC

---
+++ Modello_FOC/Foc_model_Matlab_mbd_rtw/rtGetInf.h
@@ -0,0 +1,37 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: rtGetInf.h
+ *
+ * Code generated for Simulink model 'Foc_model_Matlab'.
+ *
+ * Model version                   : 10.6
+ * Simulink Coder version          : 24.2 (R2024b) 21-Jun-2024
+ * MBDT for S32K1xx Series Version : 4.2.0 (R2016a-R2020a) 20-Jul-2020
+ * C/C++ source code generated on  : Fri Oct 11 10:23:04 2024
+ *
+ * Target selection: mbd_s32k.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef rtGetInf_h_
+#define rtGetInf_h_
+#include "rt_nonfinite.h"
+#include "rtwtypes.h"
+
+extern real_T rtGetInf(void);
+extern real32_T rtGetInfF(void);
+extern real_T rtGetMinusInf(void);
+extern real32_T rtGetMinusInfF(void);
+
+#endif                                 /* rtGetInf_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
